d8a431c5a6848502f9838174f7ce5b0f6d22c972
Joseph Vasquez Flores <josephvz17@gmail.com>
2025-12-17 11:26:00 +0100

    configuración de la API

--- scripts/ModeloALS.py
+++ scripts/ModeloALS.py
@@ -1,5 +1,8 @@
+import time
+import requests
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
+from rich.console import Console
 
 #Crear la sesion en Spark
 spark = SparkSession.builder.appName("PLFinal_ALS").getOrCreate()
@@ -51,3 +54,33 @@ ratings_total= ratings_com.unionByName(valoracionesEP)
 #Verficicación de que las valoraciones de EP ya están en el dataset final:
 num_ep = ratings_total.filter(f.col("user_id") == 666666).count()
 print(f"Valoraciones del usuario EP en el dataset final: {num_ep}")
+
+#Configuración de la API
+DIR_SALIDA = "../salida"
+API_URL = "https://api.jikan.moe/v4/anime/"
+API_DELAY = 1.0
+console = Console()
+
+def obtener_datos (anime_id):
+    url = f"{API_URL}{anime_id}"
+    time.sleep(API_DELAY)
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json().get('data', {})
+        image_url = data.get("images", {}).get("jpg", {}).get("large_image_url")
+        trailer_url = data.get("trailer", {}).get("url") or data.get("trailer", {}).get("embed_url")
+
+        return {
+            "title_english": data.get("title_english"),
+            "synopsis": data.get("synopsis", "Información no disponible"),
+            "genres": [g['name'] for g in data.get("genres", [])],
+            "studios": [s['name'] for s in data.get("studios", [])],
+            "episodes": data.get("episodes", "Información no disponible"),
+            "status": data.get("status", "Información no disponible"),
+            "score": data.get("score", "Información no disponible"),
+            "image_url": image_url or "Información no disponible",
+            "trailer_url": trailer_url or "Información no disponible"
+        }
+    except Exception as e:
+        return {"error": str(e)}
